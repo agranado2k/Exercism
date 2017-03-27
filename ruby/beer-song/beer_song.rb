7a8944273548a22f03fbd85f9b720399af1ac364
Arthur Granado <agranado@pluga.co>
2017-03-27 20:40:56 -0300

    Beer Song: Undo changes

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -11,25 +11,25 @@ class BeerSong
     first_verse.downto(last_verse).inject([]){|result, verse| result << verse(verse)}.join("\n")
   end
 
-  def verse(verse_number)
-    if verse_number == 2
+  def verse(number)
+    if number == 2
 
-      "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n" \
-      "Take one down and pass it around, #{verse_number-1} bottle of beer on the wall.\n"
+      "2 bottles of beer on the wall, 2 bottles of beer.\n" \
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
 
-    elsif verse_number == 1
+    elsif number == 1
 
       "1 bottle of beer on the wall, 1 bottle of beer.\n" \
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
 
-    elsif verse_number == 0
+    elsif number == 0
       "No more bottles of beer on the wall, no more bottles of beer.\n" \
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
 
     else
 
-      "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n" \
-      "Take one down and pass it around, #{verse_number-1} bottles of beer on the wall.\n"  
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" \
+      "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"  
     end
   end
 end
